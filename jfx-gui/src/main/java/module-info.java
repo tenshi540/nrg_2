f00189618be301eb0ed003a724ca3cff2fe4087e
Patrick Kirchknopf <pat.kir.1711@gmail.com>
2025-06-29 21:31:40 +0200

    folder

--- jfx-gui/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.energy.jfxgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.databind;
-
-
-    opens com.energy.jfxgui to javafx.fxml;
-    exports com.energy.jfxgui;
-}
